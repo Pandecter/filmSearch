9d36fbe70b361b2fa514cf6ac23574e8815f6a0f
Sergey Matveev <onlyforflash@mail.ru>
2024-02-13 18:52:30 +0300

    added footer

---
+++ src/components/AppBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar :elevation="5" rounded color="rgb(60, 60, 60)">
+      <v-row justify="center">
+        <v-col cols="2">
+          <v-app-bar-title class="myTitle">Поиск Кино</v-app-bar-title>
+        </v-col>
+        <v-col cols="1">
+          <v-icon icon="mdi-movie-search-outline" size="x-large" class="myIcon"></v-icon>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
+
+<style lang="scss">
+  .myTitle{
+    font-size: 30px !important;
+    padding-left: 65px;
+  }
+
+  .myIcon{
+    padding-right: 50px;
+  }
+</style>
---
+++ src/store/filmStorage.js
@@ -0,0 +1,23 @@
+// Utilities
+import { defineStore } from 'pinia'
+import filmData from '../../dataset/kinopoisk-1.json'
+
+export const useAppStore = defineStore('filmStorage', {
+  state: () => {
+    return {
+      posters: []
+    }
+  },
+  actions : {
+    moveToPosters(){
+      for(let i = 0; i < filmData.docs.length; i++) {
+        if('poster' in filmData.docs[i]) {
+          this.posters[i] = filmData.docs[i].poster.url
+        }
+        else {
+          this.posters[i] = null;
+        }
+      }
+    }
+  }
+})
